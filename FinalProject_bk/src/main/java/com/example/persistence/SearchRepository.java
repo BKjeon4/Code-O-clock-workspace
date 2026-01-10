c19e1a7d50e164090f35aa042c214f4000f71058
BKjeon4 <112688175+BKjeon4@users.noreply.github.com>
2026-01-10 06:05:53 -0500

    Update SearchRepository.java last change plz

--- FinalProject_bk/src/main/java/com/example/persistence/SearchRepository.java
+++ FinalProject_bk/src/main/java/com/example/persistence/SearchRepository.java
@@ -2,19 +2,18 @@ package com.example.persistence;
 
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.Repository;
 import org.springframework.data.repository.query.Param;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
-@Component
-public interface SearchRepository extends Repository<Object, Integer> {
+@Repository
+public interface SearchRepository {
 
     @Modifying
     @Transactional
     @Query(
-      value = "INSERT INTO search (keywords, m_idint) VALUES (:keywords, :memId)",
-      nativeQuery = true
+        value = "INSERT INTO search (keywords, m_idint) VALUES (:keywords, :memId)",
+        nativeQuery = true
     )
     void insertSearch(
         @Param("keywords") String keywords,
