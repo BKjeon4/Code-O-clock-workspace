914ae7543db9db48cfe09e4cdd65f3ffc9f38993
BKjeon4 <112688175+BKjeon4@users.noreply.github.com>
2026-01-10 05:41:08 -0500

    Update SearchRepository.java i m  gonna cry

--- FinalProject_bk/src/main/java/com/example/persistence/SearchRepository.java
+++ FinalProject_bk/src/main/java/com/example/persistence/SearchRepository.java
@@ -1,23 +1,23 @@
 package com.example.persistence;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
+@Repository
+public interface SearchRepository extends JpaRepository<Search, Integer> {
 
-import com.example.domain.SearchVO;
-
-public interface SearchRepository extends CrudRepository<SearchVO, Integer>{
-
-@Modifying
-@Transactional
-@Query(
-  value = "INSERT INTO search (keywords, m_idint) VALUES (:keywords, :memId)",
-  nativeQuery = true
-)
-void insertSearch(@Param("keywords") String keywords,
-                  @Param("memId") Integer memId);
-
-
-
-
+    @Modifying
+    @Transactional
+    @Query(
+      value = "INSERT INTO search (keywords, m_idint) VALUES (:keywords, :memId)",
+      nativeQuery = true
+    )
+    void insertSearch(
+        @Param("keywords") String keywords,
+        @Param("memId") Integer memId
+    );
 }
