1f3f6bfea352542e3761550b992d01e768a882bb
BKjeon4 <112688175+BKjeon4@users.noreply.github.com>
2026-01-10 05:52:26 -0500

    Update SearchRepository.java  plz

--- FinalProject_bk/src/main/java/com/example/persistence/SearchRepository.java
+++ FinalProject_bk/src/main/java/com/example/persistence/SearchRepository.java
@@ -1,14 +1,14 @@
 package com.example.persistence;
 
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.Repository;
 import org.springframework.data.repository.query.Param;
-import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
-@Repository
-public interface SearchRepository extends JpaRepository<Search, Integer> {
+@Component
+public interface SearchRepository extends Repository<Object, Integer> {
 
     @Modifying
     @Transactional
