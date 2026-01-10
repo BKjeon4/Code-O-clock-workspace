07359a58037280619abc23c36a77aa576d488a6a
BKjeon4 <112688175+BKjeon4@users.noreply.github.com>
2026-01-10 05:35:24 -0500

    Update SearchRepository.java

--- FinalProject_bk/src/main/java/com/example/persistence/SearchRepository.java
+++ FinalProject_bk/src/main/java/com/example/persistence/SearchRepository.java
@@ -8,9 +8,15 @@ import com.example.domain.SearchVO;
 
 public interface SearchRepository extends CrudRepository<SearchVO, Integer>{
 
-	//위시리스트 입력
-	@Query(value="INSERT INTO search (keywords,m_idint) VALUES(?1,?2)", nativeQuery=true)
-	void insertSearch(String keywords, Integer mIdInt);
+@Modifying
+@Transactional
+@Query(
+  value = "INSERT INTO search (keywords, m_idint) VALUES (:keywords, :memId)",
+  nativeQuery = true
+)
+void insertSearch(@Param("keywords") String keywords,
+                  @Param("memId") Integer memId);
+
 
 
 
